14e53ef4f01be56463ebe5a80659277d180dbf46
faisalkhalikkhan <83642756+faisalkhalikkhan@users.noreply.github.com>
2022-01-12 14:34:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 const URL =
-  "mongodb+srv://123:123@cluster0.ein4s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "put your mongo atlas URI";
 
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
